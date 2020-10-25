212411727e3e3e0efd7e66731d474593fdcd29fd
Christopher Bartling <chris.bartling@gmail.com>
2020-10-24 20:01:05 -0500

    Wiring in the Firebase JavaScript SDK.

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from 'firebase/app';
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import 'firebase/analytics';
+
+// Add the Firebase products that you want to use
+import 'firebase/auth';
+import 'firebase/firestore';
+
+
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "certification-exam-quizzer.firebaseapp.com",
+  databaseURL: "https://certification-exam-quizzer.firebaseio.com",
+  projectId: "certification-exam-quizzer",
+  storageBucket: "certification-exam-quizzer.appspot.com",
+  messagingSenderId: "552247886203",
+  appId: "1:552247886203:web:ace1d4bf289dc058dcbdde",
+  measurementId: "G-HN47Z70H9C"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
