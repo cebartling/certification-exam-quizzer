750c66164b01320e15af74592556774b655590da
Christopher Bartling <chris.bartling@gmail.com>
2020-11-24 00:36:06 -0600

    RSpec specs.

--- rails-api/spec/models/exam_question_response_spec.rb
+++ rails-api/spec/models/exam_question_response_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe ExamQuestionResponse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:exam_question) }
 end
