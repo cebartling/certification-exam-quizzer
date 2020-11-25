c9d27525e6eac005658c0f8c2aba5b9430fd46b2
Christopher Bartling <chris.bartling@gmail.com>
2020-11-24 21:08:34 -0600

    Adding in relationships

--- rails-api/app/graphql/types/certification_exam_type.rb
+++ rails-api/app/graphql/types/certification_exam_type.rb
@@ -5,5 +5,7 @@ module Types
     field :exam_code, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :exam_questions, Types::ExamQuestionType.connection_type, null: false
   end
 end
--- rails-api/app/graphql/types/exam_question_response_type.rb
+++ rails-api/app/graphql/types/exam_question_response_type.rb
@@ -1,11 +1,12 @@
 module Types
   class ExamQuestionResponseType < Types::BaseObject
     field :id, ID, null: false
-    field :exam_question_id, Types::UuidType, null: false
     field :response_text, String, null: false
     field :explanation_text, String, null: true
     field :correct, Boolean, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :exam_question, Types::ExamQuestionType, null: false
   end
 end
